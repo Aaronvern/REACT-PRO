501f1f5e54e52a70be7d31018d91d36396e7f3b4
aaronvern <aaronvernekar@gmail.com>
2024-04-26 01:59:52 +0530

    add changes to the theme switcher

--- pro10-darkmode-contextApi/themeToggler/src/App.jsx
+++ pro10-darkmode-contextApi/themeToggler/src/App.jsx
@@ -1,13 +1,38 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import './App.css'
+import { ThemeProvider } from './context/theme'
+import ThemeBtn from './components/Theme'
+import Card from './components/Card'
 
 function App() {
-
-
+   const [themeMode,setThemeMode]=useState("light")
+    const lightTheme = ()=> {
+      setThemeMode("light")
+    }
+    const darkTheme = ()=>{
+      setThemeMode("dark")
+    }
+    useEffect(()=>{
+      document.querySelector('html').classList.remove("light","dark")
+      document.querySelector('html').classList.add(themeMode)
+    },[themeMode])
   return (
     <>
-     
+      <ThemeProvider value={{darkTheme,themeMode,lightTheme}}>  
+        <div className="flex flex-wrap min-h-screen items-center">
+            <div className="w-full">
+                <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                   <ThemeBtn/>
+                    
+                </div>
+
+                <div className="w-full max-w-sm mx-auto">
+                  <Card/>
+                </div>
+            </div>
+        </div>
+      </ThemeProvider>
     </>
   )
 }
