62d68a08e894f30b37e20495c505a4c30404cdbd
aaronvern <aaronvernekar@gmail.com>
2024-04-27 18:14:46 +0530

    add makeup to the components

--- todoApp-rtk/todoReduxTk/src/App.jsx
+++ todoApp-rtk/todoReduxTk/src/App.jsx
@@ -1,16 +1,20 @@
-
-import './App.css'
-import AddTodo from './components/AddTodo'
-import Todos from './components/Todos'
+import React from 'react';
+import './App.css';
+import AddTodo from './components/AddTodo';
+import Todos from './components/Todos';
 
 function App() {
-  
   return (
-    <>
-      <AddTodo />
-      <Todos />
-    </>
-  )
+    <div className="bg-gray-400 min-h-screen">
+      <div className="container mx-auto px-4 py-8">
+        <h1 className="text-3xl font-bold text-center text-gray-800 mb-8">Todo App</h1>
+        <div className="max-w-md mx-auto">
+          <AddTodo />
+          <Todos />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- todoApp-rtk/todoReduxTk/src/components/AddTodo.jsx
+++ todoApp-rtk/todoReduxTk/src/components/AddTodo.jsx
@@ -1,35 +1,36 @@
-import React, {useState} from 'react'
-import {useDispatch} from 'react-redux'
-import {addTodo} from '../features/todo/todoSlice' 
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addTodo } from '../features/todo/todoSlice';
 
 function AddTodo() {
+  const [input, setInput] = useState('');
+  const dispatch = useDispatch();
 
-    const [input, setInput] = useState('')
-    const dispatch = useDispatch()
-
-    const addTodoHandler = (e) => {
-        e.preventDefault()
-        dispatch(addTodo(input))
-        setInput('')
-    }
+  const addTodoHandler = (e) => {
+    e.preventDefault();
+    dispatch(addTodo(input.trim())); // Trim whitespace from input
+    setInput('');
+  };
 
   return (
-    <form onSubmit={addTodoHandler} className="space-x-3 mt-12">
-      <input
-        type="text"
-        className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-        placeholder="Enter a Todo..."
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      />
-      <button
-        type="submit"
-        className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-      >
-        Add Todo
-      </button>
+    <form onSubmit={addTodoHandler} className="max-w-md mx-auto mt-8">
+      <div className="flex">
+        <input
+          type="text"
+          className="w-full border-gray-300 rounded-l-md py-2 px-4 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent"
+          placeholder="Enter a Todo..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button
+          type="submit"
+          className="bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-r-md"
+        >
+          Add
+        </button>
+      </div>
     </form>
-  )
+  );
 }
 
-export default AddTodo
+export default AddTodo;
